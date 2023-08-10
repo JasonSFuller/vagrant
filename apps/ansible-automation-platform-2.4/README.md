1f1228a5c3cd20dc3afe35be4137af2223db34a3
Jason Fuller <JasonSFuller@gmail.com>
2023-08-10 14:32:22 -0400

    add AAP 2.4

---
+++ apps/ansible-automation-platform-2.4/README.md
@@ -0,0 +1,83 @@
+# Ansible Automation Platform 2.2
+
+* [Red Hat Ansible Automation Platform 2.2 Installation Guide](https://access.redhat.com/documentation/en-us/red_hat_ansible_automation_platform/2.2/html/red_hat_ansible_automation_platform_installation_guide/index)
+
+
+
+## Prerequisites
+
+* A working [VirtualBox] and [Vagrant] installation, preferably on a Linux host
+* A [Red Hat developer account] for registering the RHEL guests
+* A [registry service account] for downloading the required containers
+* Enough resources on the host to run 3 fairly large guests:
+  | Hostname   | Description           | Required resources           |
+  | ---------- | --------------------- | ---------------------------- |
+  | ctl.local  | Automation controller | 4 CPU, 16 Gb RAM, 20 Gb disk |
+  | hub.local  | Automation hub        | 4 CPU, 16 Gb RAM, 20 Gb disk |
+  | mgmt.local | Management node       | 2 CPU, 4 Gb RAM, 20 Gb disk  |
+
+[VirtualBox]: https://www.virtualbox.org/
+[Vagrant]: https://www.vagrantup.com/
+[Red Hat developer account]: https://developers.redhat.com/
+[registry service account]: https://access.redhat.com/terms-based-registry/
+
+
+
+## Installation
+
+Download the **bundle** and save it to the same directory as the [`Vagrantfile`](Vagrantfile):
+
+* <https://access.redhat.com/downloads/content/480/ver=2.2/rhel---9/2.2/x86_64/product-software>
+
+Your directory should look like this:
+
+```text
+$ ls -lad ansible-automation-platform-setup-bundle-* Vagrantfile
+-rw-r--r--. 1 vagrant vagrant 1472877795 Nov 10 14:06 ansible-automation-platform-setup-bundle-2.2.1-1.1.tar.gz
+-rw-r--r--. 1 vagrant vagrant        124 Nov 10 14:09 ansible-automation-platform-setup-bundle-2.2.1-1.1.tar.gz.sha256
+-rw-r--r--. 1 vagrant vagrant       2290 Nov 10 19:36 Vagrantfile
+```
+
+Create a `.env` file.  
+
+> :pencil2: **NOTE:** This is used by the vagrant-registration plugin to
+> register the RHEL boxes (for licensing, repository access, etc).
+
+```bash
+cp .env.sample.txt .env
+vim .env
+```
+
+Create a `secrets.yaml` file.
+
+> :pencil2: **NOTE:** This is used by `setup.sh` (the AAP installer) and
+> separates your secret values from non-secret config--which is safe to store in
+> source control.
+
+```bash
+cp secrets.yaml.sample.txt secrets.yaml
+vim secrets.yaml
+```
+
+Bring up the Vagrant machines:
+
+```bash
+vagrant up
+```
+
+## Troubleshooting
+
+From the management node, validate Ansible can log in to all inventory hosts.
+
+```bash
+# vagrant ssh mgmt # if you haven't already
+cd /home/vagrant/ansible-automation-platform-setup-bundle-*/
+sudo ansible -m ping -i ./inventory all
+```
+
+Re-run the installation script manually.
+
+```bash
+cd /home/vagrant/ansible-automation-platform-setup-bundle-*/
+sudo ./setup.sh -e '@/vagrant/secrets.yaml' -i ./inventory
+```
---
+++ apps/ansible-automation-platform-2.4/install.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+function error { echo "ERROR: $*" >&2; exit 1; }
+
+
+
+################################################################################
+#  Pre-flight checks
+################################################################################
+
+# We're making a few assumptions in this script (like password-less sudo
+# access), so make sure we're logged in as the Vagrant user.
+if [[ "$(id -un)" != "vagrant" ]]; then
+  error "must run as the vagrant user"
+fi
+
+# Abort if vagrant-sshfs fails to mount /vagrant, since we need it to copy
+# files.
+if ! mountpoint /vagrant >/dev/null; then
+  error 'could not find /vagrant mount point (check sshfs?)'
+fi
+
+# Abort if the '.env' or 'secrets.yaml' files are missing.
+if [[ ! -f /vagrant/.env ]]; then
+  error 'missing .env file (see README.md)'
+fi
+if [[ ! -f /vagrant/secrets.yaml ]]; then
+  error 'missing secrets.yaml file (see README.md)'
+fi
+
+
+################################################################################
+#  Install
+################################################################################
+
+# Copy the install bundle and checksum to the Vagrant user's home directory.
+cd ~ || error 'could not find the vagrant home dir (uh, what?)'
+cp /vagrant/ansible-automation-platform-setup-bundle-* .
+
+# Validate the install bundle's checksum.
+if ! sha256sum -c ansible-automation-platform-setup-bundle-*.sha256; then
+  error "invalid checksum"
+fi
+
+# Extract the tarball.
+tar xf ansible-automation-platform-setup-bundle-*.tar.gz
+cd ansible-automation-platform-setup-bundle-*/ \
+  || error "could not find the installation directory"
+
+# Since the installer (setup.sh) is simply a wrapper for Ansible playbooks, it
+# expects the inventory file to be in a specific location relative to itself and
+# the other directories, so symlink it here.
+if [[ ! -L inventory ]]; then
+  if [[ -f inventory ]]; then mv inventory{,.orig}; fi
+  ln -s /vagrant/inventory .
+fi
+
+# Validate Ansible can reach all the *.local hosts in the inventory.
+# * NOTE: This step is important because it also creates the
+#   /root/.ssh/known_hosts file to avoid typing "yes" to accept the SSH
+#   fingerprint over and over during the install.
+# * TODO: Grep'ing the inventory is a bit clumsy and error prone, but Ansible
+#   has not been installed by setup.sh yet.
+sudo install -m 0755 -d "/root/.ssh/"
+grep -oE '\b[A-Za-z0-9\-\.]*\.local\b' /vagrant/inventory \
+  | sort -u \
+  | ssh-keyscan -f - 2>/dev/null \
+  | sudo install -m 0600 /dev/stdin "/root/.ssh/known_hosts"
+
+# Run the installer (as root).
+printf "Running setup.sh.  This will take a few minutes.  To check progress, in another terminal:\n"
+printf "  vagrant ssh mgmt\n"
+printf "  sudo tail -F %s/%s\n" "$(realpath .)" "setup.out\n"
+sudo ./setup.sh -e '@/vagrant/secrets.yaml' > setup.out
